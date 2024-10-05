ca2f31723a7bc20c3e5d0243d879c8cd29a844d7
Victor Notaro <128001136+victornotaro1@users.noreply.github.com>
2024-10-05 15:30:01 -0300

    Create Form-games

---
+++ application/controllers/Games.php
@@ -0,0 +1,69 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Games extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("games_model");
+
+    }
+
+    public function index() {
+
+        $data["games"] = $this->games_model->index();
+        $data["title"] = "Games - CodeIgniter";
+        
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/nav-top', $data);
+        $this->load->view('pages/games', $data);
+        $this->load->view('templates/footer', $data);
+        $this->load->view('templates/js', $data);
+
+    }
+
+    public function new()
+    {
+        $data["title"] = "Games - CodeIgniter";
+        
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/nav-top', $data);
+        $this->load->view('pages/form-games', $data);
+        $this->load->view('templates/footer', $data);
+        $this->load->view('templates/js', $data);
+    }
+
+    public function Store()
+    {
+        $game = $_POST;
+        $game["user_id"] = '1';
+        $this->games_model->store($game);
+        redirect("dashboard");
+    }
+
+    public function edit($id)
+    {
+        $data["game"] = $this->games_model->show($id);
+        $data["title"] = "Editar Games - CodeIgniter";
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/nav-top', $data);
+        $this->load->view('pages/form-games', $data);
+        $this->load->view('templates/footer', $data);
+        $this->load->view('templates/js', $data);
+    }
+
+    public function update($id)
+    {
+        $game = $_POST;
+        $this->games_model->update($id, $game);
+        redirect("games");
+    }
+
+    public function delete($id)
+    {
+        $this->games_model->destroy($id);
+        redirect("games");
+    }
+
+}
